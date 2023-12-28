172b86c081dfa1d477b6a504d5e1a4513437a6c1
selecter git user <git-user@selectel.ru>
2023-12-28 21:42:18 +0800

    gggg

--- src/components/EndGameModal/EndGameModal.jsx
+++ src/components/EndGameModal/EndGameModal.jsx
@@ -1,53 +1,62 @@
-import styles from "./EndGameModal.module.css"
-import { Button } from "../Button/Button"
-import deadImageUrl from "./images/dead.png"
-import celebrationImageUrl from "./images/celebration.png"
-import { Link } from "react-router-dom"
-import { postNewLeader } from "../../api"
-import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
-
-
+import styles from "./EndGameModal.module.css";
+import { Button } from "../Button/Button";
+import deadImageUrl from "./images/dead.png";
+import celebrationImageUrl from "./images/celebration.png";
+import { Link } from "react-router-dom";
+import { postNewLeader } from "../../api";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 export function EndGameModal({ isWon, gameDurationSeconds, gameDurationMinutes, onClick }) {
-  useEffect(()=>{console.log(gameNumber)},[])
-const gameNumber= useSelector(state=>state.game.gameNumber) 
-const gameDuration =gameDurationMinutes*60+gameDurationSeconds 
+  const gameNumber = useSelector(state => state.game.gameNumber);
+  const gameDuration = gameDurationMinutes * 60 + gameDurationSeconds;
+  const [userName, setUserName] = useState('Пользователь');
 
-  
-  const[userName,setUserName]=useState('Пользователь')
-  
-  const title = isWon ? (gameNumber===3?"Вы попали в наши лидеры":"Вы победили!") : "Вы проиграли!"
-  const imgSrc = isWon ? celebrationImageUrl : deadImageUrl
-  const imgAlt = isWon ? "celebration emodji" : "dead emodji"
+  useEffect(() => {
+    if (gameNumber !== undefined) {
+      console.log(gameNumber);
+    }
+  }, [gameNumber]);
 
+  const title = isWon ? (gameNumber === 3 ? "Вы попали в наши лидеры" : "Вы победили!") : "Вы проиграли!";
+  const imgSrc = isWon ? celebrationImageUrl : deadImageUrl;
+  const imgAlt = isWon ? "celebration emodji" : "dead emodji";
+
+  const handleLeaderboardClick = () => {
+    if (isWon && gameNumber === 3) {
+      postNewLeader(userName, gameDuration);
+    }
+  };
 
   return (
     <div className={styles.modal}>
       <img className={styles.image} src={imgSrc} alt={imgAlt} />
       <h2 className={styles.title}>{title}</h2>
       {(isWon && gameNumber === 3) && (
-      <input
-        onChange={(event) => {
-          setUserName(event.target.value);
-        }}
-        className={styles.input}
-        type="text"
-        placeholder="Введите ваше имя"
-      />
-    )}
+        <input
+          onChange={(event) => {
+            setUserName(event.target.value);
+          }}
+          className={styles.input}
+          type="text"
+          placeholder="Введите ваше имя"
+        />
+      )}
       <p className={styles.description}>Затраченное время:</p>
       <div className={styles.time}>
-      {gameDurationMinutes.toString().padStart(2, "0")}.{gameDurationSeconds.toString().padStart(2, "0")}
-    </div>
-<div onClick={(isWon===true)
-   & gameNumber===3 
-   ?()=>postNewLeader(userName,gameDuration):null}>
-      <Button onClick={onClick}>Начать сначала</Button>
+  {gameDurationMinutes && gameDurationSeconds && gameDurationMinutes.toString().padStart(2, "0")}.{gameDurationSeconds.toString().padStart(2, "0")}
 </div>
-<div>
-        <Link to="/game/leaderboard" onClick={() => postNewLeader(userName, gameDuration)}>Перейти к нашим лидерам</Link>
+      <div>
+        <Button onClick={onClick}>Начать сначала</Button>
+      </div>
+      <div>
+        {(isWon && gameNumber === 3) && (
+          <Button onClick={handleLeaderboardClick}>Перейти к нашим лидерам</Button>
+        )}
+        {!isWon && (
+          <Link to="/game/leaderboard">Перейти к нашим лидерам</Link>
+        )}
       </div>
     </div>
-  )
+  );
 }
